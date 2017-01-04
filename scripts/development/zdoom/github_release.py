e0f0d3494babc005a642f3aeb2dd67234c3f46cb
alexey.lysiuk <alexey.lysiuk@gmail.com>
2017-01-04 11:16:28 +0200

    * better feedback and error reporting in GitHub release script

--- scripts/development/zdoom/github_release.py
+++ scripts/development/zdoom/github_release.py
@@ -2,10 +2,11 @@
 
 import sys
 import os
+import traceback
 import github3
 
 if 8 != len(sys.argv):
-    print('Usage: github-release <user> <token> <repository> <tag> <name> <description> <file-to-upload>')
+    print('Usage: github_release <user> <token> <repository> <tag> <name> <description> <file-to-upload>')
     sys.exit(1)
 
 class Config(object):
@@ -20,17 +21,22 @@ config.name = sys.argv[5]
 config.desc = sys.argv[6].replace(r'\n', '\n')
 config.path = sys.argv[7]
 
-print('Uploading GitHub release...')
-
 try:
     gh = github3.login(config.user, token=config.token)
+
+    print('Connecting to GitHub...')
     repo = gh.repository(config.user, config.repo)
+
+    print('Creating GitHub release...')
     release = repo.create_release(config.tag, name=config.name, body=config.desc, prerelease=True)
 
     with open(config.path, 'rb') as asset_file:
         asset_name = os.path.basename(config.path)
+
+        print('Uploading GitHub release asset...')
         release.upload_asset('application/octet-stream', asset_name, asset_file)
 
 except Exception as ex:
-    print('Failed to create GitHub release with error:\n' + ex.message)
+    print('\nERROR: Failed to create GitHub release\n')
+    traceback.print_exc(file=sys.stdout)
     sys.exit(1)
