1cb9d50b0153f97b88af172a85a98da9357ebed8
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-06-08 22:26:42 +0300

    * do not include docs in *zdoom devbuild
    licenses are still copied to disk image

--- scripts/development/zdoom/devbuild.sh
+++ scripts/development/zdoom/devbuild.sh
@@ -97,7 +97,7 @@ INFO_PLIST_PATH=${BUNDLE_PATH}/Contents/Info.plist
 
 mkdir "${DIST_DIR}"
 cp -R ${ZDOOM_PROJECT_LOW}.app "${BUNDLE_PATH}"
-cp -R "${ZDOOM_DIR}docs" "${DIST_DIR}Docs"
+cp -R "${ZDOOM_DIR}docs/licenses" "${DIST_DIR}Licenses"
 ln -s /Applications "${DIST_DIR}/Applications"
 
 plutil -replace LSMinimumSystemVersion -string "${ZDOOM_OS_MIN_VER}" "${INFO_PLIST_PATH}"
