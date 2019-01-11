76c96688995d4568a900462b6d0d70ce16d01f76
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-01-11 16:02:53 +0200

    * silent pk3 creation in *zdoom devbuild script

--- scripts/development/zdoom/devbuild.sh
+++ scripts/development/zdoom/devbuild.sh
@@ -77,6 +77,7 @@ LINKER_FLAGS=${OTHER_LIBS}\ ${FRAMEWORKS}
 	-DDYN_FLUIDSYNTH=NO                                  \
 	-DFORCE_INTERNAL_ZLIB=YES                            \
 	-DFORCE_INTERNAL_GME=YES                             \
+	-DPK3_QUIET_ZIPDIR=YES                               \
 	-DOPENAL_INCLUDE_DIR="${OPENAL_DIR}include"          \
 	-DOPENAL_LIBRARY="${OPENAL_DIR}lib/libopenal.a"      \
 	-DMPG123_INCLUDE_DIR="${MPG123_DIR}include"          \
