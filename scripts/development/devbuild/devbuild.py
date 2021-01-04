9386c1cf9d2700e3b9ecd88cd1e854c35926536e
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-01-04 11:01:51 +0200

    devbuild script: rename variables according to todos

--- scripts/development/devbuild/devbuild.py
+++ scripts/development/devbuild/devbuild.py
@@ -31,34 +31,27 @@ class BuildState:
 
     TARGETS = {target.name.lower(): target for target in _TARGETS_LIST}
 
-    def __init__(self, target: str, commit: str):
-        target = target.lower()
-        self.target = BuildState.TARGETS[target]
-
-        self.zdoom_project = self.target.name  # TODO: rename to 'target'
-        self.commit = commit
-        self.zdoom_os_min_ver = '10.9'  # TODO: rename to 'target_min_ver'
-
-        # TODO: rename to 'target_lower'
-        self.zdoom_project_low = target
+    def __init__(self, target: str, checkout: str):
+        self.target_name_lower = target.lower()
+        self.target = BuildState.TARGETS[self.target_name_lower]
+        self.target_os_version = '10.9'
+        self.checkout = checkout
 
         # Source directories
         self.script_dir = os.path.dirname(os.path.abspath(__file__)) + os.sep
         self.src_base_dir = '/Volumes/Storage/Work/devbuilds/'
         self.src_deps_dir = self.src_base_dir + 'zdoom-macos-deps' + os.sep
-        # TODO: rename to 'src_target_dir'
-        self.src_zdoom_dir = self.src_base_dir + self.zdoom_project_low + os.sep
+        self.src_target_dir = self.src_base_dir + self.target_name_lower + os.sep
         self.src_widepix_dir = self.src_base_dir + 'WidePix' + os.sep
 
         # Build directories
-        self.base_dir = f'/Volumes/ramdisk/{self.zdoom_project_low}-devbuild/'
+        self.base_dir = f'/Volumes/ramdisk/{self.target_name_lower}-devbuild/'
         self.deps_dir = self.base_dir + 'deps' + os.sep
-        # TODO: rename to 'target_dir'
-        self.zdoom_dir = self.base_dir + self.zdoom_project_low + os.sep
+        self.target_dir = self.base_dir + self.target_name_lower + os.sep
         self.build_dir = self.base_dir + 'build' + os.sep
         self.dist_dir = self.base_dir + 'dist' + os.sep
 
-        self.bundle_name = self.zdoom_project + '.app'
+        self.bundle_name = self.target.name + '.app'
         self.bundle_path = self.dist_dir + self.bundle_name
 
         self.disk_image_name = None
@@ -68,15 +61,14 @@ class BuildState:
         self.disk_image_checksum = None
 
         self.deps_commit = None
-        self.zdoom_version = None
-        self.zdoom_commit = None
+        self.target_version = None
+        self.target_commit = None
 
         self.deployment_config = {}
 
         # LZDoom doesn't have separate devbuilds repository
-        # TODO: rename to 'target_devbuilds'
-        self.zdoom_devbuilds = 'gzdoom' if self.zdoom_project_low == 'lzdoom' else self.zdoom_project_low
-        self.zdoom_devbuilds += '-macos-devbuilds'
+        self.target_devbuilds = 'gzdoom' if self.target_name_lower == 'lzdoom' else self.target_name_lower
+        self.target_devbuilds += '-macos-devbuilds'
 
     @staticmethod
     def _run(args, cwd: str) -> str:
@@ -85,7 +77,7 @@ class BuildState:
     def prepare_directories(self):
         args = ('git', 'fetch', '--all', '--tags')
         subprocess.check_call(args, cwd=self.src_deps_dir)
-        subprocess.check_call(args, cwd=self.src_zdoom_dir)
+        subprocess.check_call(args, cwd=self.src_target_dir)
         subprocess.check_call(args, cwd=self.src_widepix_dir)
 
         os.makedirs(self.base_dir)
@@ -93,30 +85,28 @@ class BuildState:
         clone_args = ('git', 'clone', '-s')
         args = clone_args + (self.src_deps_dir, self.deps_dir)
         subprocess.check_call(args, cwd=self.base_dir)
-        args = clone_args + (self.src_zdoom_dir, self.zdoom_dir)
+        args = clone_args + (self.src_target_dir, self.target_dir)
         subprocess.check_call(args, cwd=self.base_dir)
 
-        args = ('git', 'checkout', self.zdoom_os_min_ver)
+        args = ('git', 'checkout', self.target_os_version)
         subprocess.check_call(args, cwd=self.deps_dir)
 
-        if self.commit:
-            args = ('git', 'checkout', self.commit)
-            subprocess.check_call(args, cwd=self.zdoom_dir)
+        if self.checkout:
+            args = ('git', 'checkout', self.checkout)
+            subprocess.check_call(args, cwd=self.target_dir)
 
         args = ('git', 'submodule', 'update', '--init', '--recursive', '--reference', self.src_widepix_dir)
-        subprocess.check_call(args, cwd=self.zdoom_dir)
+        subprocess.check_call(args, cwd=self.target_dir)
 
     def setup_target(self):
         log_commit_args = ('git', 'log', '--pretty=format:%h', '-n', '1')
         self.deps_commit = BuildState._run(log_commit_args, self.deps_dir)
 
         args = ('git', 'describe', '--tags')
-        # TODO: rename
-        self.zdoom_version = BuildState._run(args, self.zdoom_dir)
-        # TODO: rename
-        self.zdoom_commit = BuildState._run(log_commit_args, self.zdoom_dir)
+        self.target_version = BuildState._run(args, self.target_dir)
+        self.target_commit = BuildState._run(log_commit_args, self.target_dir)
 
-        self.disk_image_name = self.zdoom_project + '-' + self.zdoom_version
+        self.disk_image_name = self.target.name + '-' + self.target_version
         self.disk_image_filename = self.disk_image_name.lower() + '.dmg'
         self.disk_image_path = self.base_dir + self.disk_image_filename
 
@@ -127,22 +117,22 @@ class BuildState:
         builder = build_module.__dict__['Builder']
 
         args = (
-            f'--source-path={self.zdoom_dir}',
+            f'--source-path={self.target_dir}',
             f'--output-path={self.dist_dir}',
-            f'--sdk-path-x64={self.src_base_dir}/macos_sdk/MacOSX{self.zdoom_os_min_ver}.sdk',
+            f'--sdk-path-x64={self.src_base_dir}/macos_sdk/MacOSX{self.target_os_version}.sdk',
             # ARM64 target uses the latest 11.x SDK for now
         )
         builder(args).run()
 
     def prepare_app_bundle(self):
-        output_path = self.dist_dir + os.sep + self.zdoom_project_low + os.sep
-        os.rename(output_path + self.zdoom_project_low + '.app', self.bundle_path)
+        output_path = self.dist_dir + os.sep + self.target_name_lower + os.sep
+        os.rename(output_path + self.target_name_lower + '.app', self.bundle_path)
         os.rmdir(output_path)
 
         apps = '/Applications'
         os.symlink(apps, self.dist_dir + apps)
 
-        src_licenses = self.zdoom_dir + 'docs/licenses'
+        src_licenses = self.target_dir + 'docs/licenses'
         if os.path.exists(src_licenses):
             dst_target_licenses = self.dist_dir + 'Licenses'
             shutil.copytree(src_licenses, dst_target_licenses)
@@ -154,9 +144,9 @@ class BuildState:
         with open(info_plist, 'rb') as f:
             target_plist = plistlib.load(f)
 
-        target_plist['LSMinimumSystemVersion'] = self.zdoom_os_min_ver
-        target_plist['CFBundleName'] = self.zdoom_project
-        target_plist['CFBundleShortVersionString'] = self.zdoom_version
+        target_plist['LSMinimumSystemVersion'] = self.target_os_version
+        target_plist['CFBundleName'] = self.target.name
+        target_plist['CFBundleShortVersionString'] = self.target_version
         target_plist['CFBundleIdentifier'] = self.target.identifier
         target_plist['ZDependenciesRepositoryCommit'] = self.deps_commit
 
@@ -165,10 +155,10 @@ class BuildState:
 
     def compress_bundle(self):
         # create .tar.bz2 containing app bundle for "special" builds
-        if not self.commit:
+        if not self.checkout:
             return
 
-        archive_path = f'{self.base_dir}{self.zdoom_project_low}-{self.commit}.tar.bz2'
+        archive_path = f'{self.base_dir}{self.target_name_lower}-{self.target_commit}.tar.bz2'
 
         with tarfile.open(archive_path, 'w:bz2') as archive:
             old_cwd = os.getcwd()
@@ -212,18 +202,18 @@ class BuildState:
         self.disk_image_checksum = hasher.hexdigest()
 
     def update_devbuilds_repository(self):
-        devbuilds_path = self.src_base_dir + self.zdoom_devbuilds + os.sep
+        devbuilds_path = self.src_base_dir + self.target_devbuilds + os.sep
         readme_filename = 'README.md'
         devbuilds_readme = devbuilds_path + readme_filename
 
         with open(devbuilds_readme, 'r') as f:
             readme_content = f.readlines()
 
-        repo_url = 'https://github.com/alexey-lysiuk/' + self.zdoom_devbuilds
-        download_url = f'{repo_url}/releases/download/{self.zdoom_version}/{self.disk_image_filename}'
+        repo_url = 'https://github.com/alexey-lysiuk/' + self.target_devbuilds
+        download_url = f'{repo_url}/releases/download/{self.target_version}/{self.disk_image_filename}'
 
         table_top = readme_content.index('|---|---|\n') + 1
-        new_entry = f'|[`{self.zdoom_version}`]({download_url})|`{self.disk_image_checksum}`|\n'
+        new_entry = f'|[`{self.target_version}`]({download_url})|`{self.disk_image_checksum}`|\n'
         readme_content.insert(table_top, new_entry)
 
         with open(devbuilds_readme, 'w') as f:
@@ -232,7 +222,7 @@ class BuildState:
         args = ('git', 'add', readme_filename)
         subprocess.check_call(args, cwd=devbuilds_path)
 
-        args = ('git', 'commit', '-m', '+ ' + self.zdoom_version)
+        args = ('git', 'commit', '-m', '+ ' + self.target_version)
         subprocess.check_call(args, cwd=devbuilds_path)
 
         args = ('git', 'push')
@@ -271,19 +261,19 @@ class BuildState:
 
         print('Connecting to GitHub...')
         session = github3.login(user, token=token)
-        repository = session.repository(user, self.zdoom_devbuilds)
+        repository = session.repository(user, self.target_devbuilds)
 
         args = ('git', 'remote', 'get-url', 'origin')
-        target_repository_url = BuildState._run(args, self.src_zdoom_dir)
+        target_repository_url = BuildState._run(args, self.src_target_dir)
         target_repository_url = target_repository_url.replace('https://github.com/', '')
         target_repository_url = target_repository_url.replace('.git', '')
 
         print('Creating GitHub release...')
-        release_name = self.zdoom_project + ' ' + self.zdoom_version
+        release_name = self.target.name + ' ' + self.target_version
         release_description = \
-            f'Development build at {target_repository_url}@{self.zdoom_commit}\nSHA-256: {self.disk_image_checksum}'
+            f'Development build at {target_repository_url}@{self.target_commit}\nSHA-256: {self.disk_image_checksum}'
         release = repository.create_release(
-            self.zdoom_version, name=release_name, body=release_description, prerelease=True)
+            self.target_version, name=release_name, body=release_description, prerelease=True)
 
         print('Uploading GitHub release asset...')
         release.upload_asset('application/octet-stream', self.disk_image_filename, self.disk_image_content)
@@ -309,7 +299,7 @@ class BuildState:
         wait_for_prompt()
 
         directory_pattern = self.deployment_config['SFTP_DIR']
-        directory = directory_pattern % self.zdoom_project_low
+        directory = directory_pattern % self.target_name_lower
         sftp.write(f'cd {directory}\n')
         wait_for_prompt()
 
@@ -327,9 +317,9 @@ def _main():
         exit(1)
 
     target = sys.argv[1]
-    commit = sys.argv[2] if len(sys.argv) > 2 else None
+    checkout = sys.argv[2] if len(sys.argv) > 2 else None
 
-    state = BuildState(target, commit)
+    state = BuildState(target, checkout)
     state.prepare_directories()
     state.setup_target()
 
