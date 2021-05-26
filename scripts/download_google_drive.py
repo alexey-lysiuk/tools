c94aee8850ace0c9944dee026f407812a64cd109
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-05-26 17:16:43 +0300

    support download of folder in google drive download script

--- scripts/download_google_drive.py
+++ scripts/download_google_drive.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 import cgi
+import html.parser
 import re
 import os
 import sys
 import urllib.parse
+
+import pyjsparser
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/development/devbuild')
 sys.dont_write_bytecode = True
@@ -12,12 +15,58 @@ sys.dont_write_bytecode = True
 import requests
 
 
+class FileIdParser(html.parser.HTMLParser):
+    def __init__(self):
+        super().__init__()
+
+        self.file_ids = []
+        self._in_script = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'script':
+            self._in_script = True
+
+    def handle_endtag(self, tag):
+        self._in_script = False
+
+    def handle_data(self, data: str):
+        if not self._in_script:
+            return
+
+        if not data.startswith('AF_initDataCallback'):
+            return
+
+        ast = pyjsparser.parse(data)
+        props = ast['body'][0]['expression']['arguments'][0]['properties']
+        prop_key = None
+        prop_data = None
+
+        for prop in props:
+            current_key = prop['key']['name']
+
+            if current_key == 'key':
+                prop_key = prop['value']['value']
+            elif current_key == 'data':
+                prop_data = prop['value']['elements']
+
+        if prop_key != 'ds:4' or not prop_data:
+            return
+
+        for entry in prop_data[4]['elements']:
+            file_id = entry['elements'][0]['value']
+            self.file_ids.append(file_id)
+
+    def error(self, message):
+        pass
+
+
 # Based on https://stackoverflow.com/a/39225039
 
 def _get_file_id(request: str) -> str:
     url_patterns = (
         r'https://drive.google.com/file/d/([^/]+)/?.*',
-        r'https://drive.google.com/open\?id=([^/]+)'
+        r'https://drive.google.com/open\?id=([^/]+)',
+        r'https://drive.google.com/drive/folders/([^/]+)',
     )
 
     for pattern in url_patterns:
@@ -48,6 +97,10 @@ def _download(request: str) -> None:
     session = requests.Session()
     response = session.get(url, stream=True)
 
+    if response.status_code != 200:
+        _download_directory(file_id)
+        return
+
     token = None
 
     for key, value in response.cookies.items():
@@ -74,6 +127,19 @@ def _download(request: str) -> None:
                 sys.stdout.flush()
 
     print('')
+
+
+def _download_directory(dir_id: str) -> None:
+    url = 'https://drive.google.com/drive/folders/' + dir_id
+    session = requests.Session()
+    response = session.get(url, stream=True)
+    content = response.content.decode(response.encoding)
+
+    parser = FileIdParser()
+    parser.feed(content)
+
+    for file_id in parser.file_ids:
+        _download(file_id)
 
 
 def main():
