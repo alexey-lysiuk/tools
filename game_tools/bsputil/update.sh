5a330d47b88b86826484a98e95babcfc5a3d9f78
alexey.lysiuk <alexey.lysiuk@gmail.com>
2021-09-28 12:45:07 +0300

    add bsputil update script

---
+++ game_tools/bsputil/update.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -o errexit
+
+cd "${0%/*}"
+
+for SUBDIR in src/common src/light tmp; do
+	if [ -e ${SUBDIR} ]; then
+		rm -r ${SUBDIR}
+	fi
+done
+
+mkdir -p src/common src/light tmp
+
+COMMIT=af7e3308f635aa486bae602986b12a3fb78dde60
+
+cd tmp
+curl -JOL https://github.com/ericwa/ericw-tools/archive/${COMMIT}.zip
+tar -xf ericw-tools-${COMMIT}.zip
+
+cd ericw-tools-${COMMIT}/common
+mv bspfile.cc bsputils.cc cmdlib.cc entdata.cc log.cc mathlib.cc polylib.cc threads.cc ../../../src/common
+
+cd ../include/common
+mv entdata.h bspfile.hh bsputils.hh cmdlib.hh log.hh mathlib.hh polylib.hh qvec.hh threads.hh ../../../../src/common
+
+cd ../light
+mv entities.hh imglib.hh light.hh litfile.hh ltface.hh settings.hh trace.hh ../../../../src/light
+
+cd ../../bsputil
+mv bsputil.cc decompile.cpp decompile.h ../../../src
+
+cd ../..
+rm -r ericw-tools-${COMMIT} ericw-tools-${COMMIT}.zip
